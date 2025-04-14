0a64621aae24d2bbb61340d53ceb41addd8893c1
abigalw <abigalw14@gmail.com>
2025-04-14 17:14:24 -0600

    update

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
 const db = mysql.createPool({
-  host: "srv1134.hstgr.io",
+  host: "srv561.hstgr.io",
   user: "u884692087_evangadiuser",
   password: process.env.DB_PASSWORD,
   database: "u884692087_evangadidb",
